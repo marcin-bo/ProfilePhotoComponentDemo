b903c52281749864f624dad246043b61e659fbfb
Marcin Borek <26303487+marcin-bo@users.noreply.github.com>
2023-10-23 15:08:21 +0200

    Add project files

---
+++ ProfilePhotoComponentDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ProfilePhotoComponentDemo
+//
+//  Created by Marcin Borek on 23/10/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
